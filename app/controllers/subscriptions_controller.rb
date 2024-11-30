894ccc47f57e29125f8a50952b4e5246c6809edf
Sameed Atif <sameedatif68@gmail.com>
2024-12-01 00:06:12 +0500

    Redirected based to root on success

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,13 +7,14 @@ class SubscriptionsController < ApplicationController
     create_paddle_products!
     paddle_customer = current_user.paddle_customers.find_or_create_by!(customer_information)
     @paddle_subscription = create_paddle_subscription!(paddle_customer)
-    redirect_to rooms_path, notice: "You have successfully subscribed to the plan"
+
+    render json: { redirect_url: rooms_path }, status: :ok
   end
 
   private
 
   def customer_information
-    customer_information = params.dig("data", "customer").transform_keys("id" => "customer_id")
+    customer_information = params.dig("data", "customer").permit!.to_h.transform_keys("id" => "customer_id")
     address_information = customer_information.delete("address")
     address_information.delete("id")
     customer_information.delete("business")
@@ -22,7 +23,7 @@ class SubscriptionsController < ApplicationController
 
   def create_paddle_products!
     params.dig("data", "items").each do |item|
-      paddle_product = item.dig("product").transform_keys("id" => "product_id")
+      paddle_product = item.dig("product").permit!.to_h.transform_keys("id" => "product_id")
       paddle_product.delete("image_url")
       PaddleProduct.find_or_create_by!(paddle_product)
     end
@@ -31,9 +32,12 @@ class SubscriptionsController < ApplicationController
   def create_paddle_subscription!(paddle_customer)
     subscription_information = params.dig("data").except("id", "customer", "items", "settings", "recurring_totals", "custom_data")
     totals =subscription_information.delete("totals").except("balance", "credit")
-    payment_method = subscription_information.dig("payment", "method_details").transform_keys("type" => "payment_method_type")
+    payment_method = subscription_information.dig("payment", "method_details").permit!.to_h.transform_keys("type" => "payment_method_type")
     card_details = payment_method.delete("card").transform_keys { |key| "card_#{key}" }
-    subscription_information.merge!(totals, payment_method, card_details, paddle_customer_id: paddle_customer.id)
-    current_user.paddle_subscription.find_or_create_by!(subscription_information.except("payment"))
+    payload = subscription_information.to_unsafe_h.merge(totals.to_unsafe_h, payment_method, card_details).except("payment")
+    obj = current_user.paddle_subscriptions.find_or_initialize_by(payload)
+    obj.paddle_customer = paddle_customer
+
+    obj.save!
   end
 end
