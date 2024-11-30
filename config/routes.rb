00857dd2622787edf3f377961fe9cae468bddbd3
Sameed Atif <sameedatif68@gmail.com>
2024-11-30 16:42:35 +0500

    WIP checkout page flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -22,23 +21,25 @@ Rails.application.routes.draw do
 
   match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
 
-
   resources :organizations do
     collection do
       get 'settings'
     end
   end
+
   resources :messages do
     collection do
       get 'settings'
     end
   end
+
   resources :rooms, only: [:show, :update, :index] do
     collection do
       get 'appearance_setting'
     end
     resources :messages
   end
+
   resources :notifications, only: [] do
     collection do
       get 'settings'
@@ -46,6 +47,8 @@ Rails.application.routes.draw do
   end
 
   resources :contacts, only: [:create]
+
+  resources :subscriptions, only: [:new]
 
   get '/settings' => 'rooms#settings', as: :settings
 
