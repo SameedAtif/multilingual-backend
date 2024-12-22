32e579b05b55daf0cee4bb98aafc2a5fc92b4838
Sameed Atif <sameedatif68@gmail.com>
2024-12-22 21:10:31 +0500

    MOVED TO ENV

--- app/jobs/fetch_paddle_subscription_job.rb
+++ app/jobs/fetch_paddle_subscription_job.rb
@@ -7,9 +7,9 @@ class FetchPaddleSubscriptionJob
 
   def perform(transaction_id)
     response = HTTParty.get(
-      "https://sandbox-api.paddle.com/transactions/#{transaction_id}",
+      "#{ENV['PADDLE_BASE_URL']}/transactions/#{transaction_id}",
       headers: {
-        'Authorization' => "Bearer 8add296a79cdf921c909c38efb005d78ae4ab24374207e5d92"
+        'Authorization' => "Bearer #{ENV['PADDLE_AUTH']}"
       }
     ).parsed_response
 
--- app/services/message_interpretation_service.rb
+++ app/services/message_interpretation_service.rb
@@ -4,7 +4,7 @@ class MessageInterpretationService
     receiver = message.room.other_participant(sender)
     # 1 Users source language is the other users target language
     res = HTTParty.post(
-      "http://127.0.0.1:5000/translate/#{receiver.user.language.downcase}",
+      "#{ENV['TRANSLATE_URL']}/translate/#{receiver.user.language.downcase}",
       body: {
         sentences: [message.source_text]
       }.to_json,
