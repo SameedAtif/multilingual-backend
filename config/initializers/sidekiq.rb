911639a53b17a53b0a956fdd988c0f1722f7cb49
Sameed Atif <sameedatif68@gmail.com>
2025-01-19 13:55:08 +0500

    Fixed sidekiq issue

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL_SIDEKIQ'] || "redis://127.0.0.1:6379" }
 end
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL_SIDEKIQ'] || "redis://127.0.0.1:6379" }
+end
